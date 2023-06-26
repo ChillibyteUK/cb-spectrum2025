e07e635353993a0bd0358628a1d529989a3e416c
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-26 15:59:34 +0100

    clients slider

--- page-templates/blocks/cb_client_slider.php
+++ page-templates/blocks/cb_client_slider.php
@@ -1,5 +1,6 @@
 <section class="clients">
     <div class="container">Our Clients</div>
+    <div class="clients__slider">
     <?php
     while (have_rows('client_logos')) {
         the_row();
@@ -8,4 +9,47 @@
         <?php
     }
     ?>
+    </div>
 </section>
+<?php
+add_action('wp_footer', function () {
+    ?>
+<script type="text/javascript">
+jQuery(function($){
+    $('.clients__slider').slick({
+        infinite: true,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1000,
+        speed: 500,
+        pauseOnHover: false,
+        cssEase: 'ease',
+        arrows: true,
+        nextArrow: '<i class="fa fa-angle-right fa-3x"></i>',
+        prevArrow: '<i class="fa fa-angle-left fa-3x"></i>',
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    autoplay: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true
+                }
+            }
+        ]
+    });
+});
+</script>
+    <?php
+}, 9999);
+?>
