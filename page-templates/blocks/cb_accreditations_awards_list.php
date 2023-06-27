04a268e8e5ca36ec43cdef82c9c734d6626a880b
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-27 10:07:01 +0100

    awards list flex

--- page-templates/blocks/cb_accreditations_awards_list.php
+++ page-templates/blocks/cb_accreditations_awards_list.php
@@ -14,12 +14,12 @@ else {
 <section class="awards_list">
     <div class="container-xl">
         <h2 class="text-blue-400 dot mb-5"><?=$title?></h2>
-        <div class="awards_list__grid">
+        <div class="row g-4">
         <?php
         foreach ($list as $l) {
             ?>
-            <div><img src="<?=wp_get_attachment_image_url($l['logo'],'large')?>" alt=""></div>
-            <div><?=$l['description']?></div>
+            <div class="col-md-3"><img src="<?=wp_get_attachment_image_url($l['logo'],'large')?>" alt=""></div>
+            <div class="col-md-9"><?=$l['description']?></div>
             <?php
         }
         ?>
