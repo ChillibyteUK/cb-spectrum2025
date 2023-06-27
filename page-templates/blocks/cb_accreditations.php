3a55437129e18fad2447b6532aec33178a33e613
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-27 09:17:45 +0100

    awards block

--- page-templates/blocks/cb_accreditations.php
+++ page-templates/blocks/cb_accreditations.php
@@ -1,59 +1,21 @@
 <section class="accreditations py-5">
     <div class="container-xl">
         <h2 class="text-blue-400 dot mb-5">Accreditations &amp; Awards</h2>
-        <div class="accreditations__slider">
+        <div class="accreditations__grid mb-5">
             <?php
+            $c = 0;
             while(have_rows('accreditations','options')) {
                 the_row();
+                if ($c > 5) {
+                    continue;
+                }
                 ?>
-                <div class="logo">
-                    <img src="<?=wp_get_attachment_image_url(get_sub_field('logo'),'large')?>" alt="">
-                </div>
+                <img class="logo" src="<?=wp_get_attachment_image_url(get_sub_field('logo'),'large')?>" alt="">
                 <?php
+                $c++;
             }
             ?>
         </div>
+        <div class="text-center"><a class="btn btn-primary" href="/accreditations-awards/">Find out more</a></div>
     </div>
 </section>
-<?php
-add_action('wp_footer', function () {
-    ?>
-<script type="text/javascript">
-jQuery(function($){
-    $('.accreditations__slider').slick({
-        infinite: true,
-        slidesToShow: 8,
-        slidesToScroll: 1,
-        autoplay: false,
-        autoplaySpeed: 1000,
-        speed: 500,
-        pauseOnHover: false,
-        cssEase: 'ease',
-        arrows: true,
-        nextArrow: '<i class="fa fa-angle-right fa-2x"></i>',
-        prevArrow: '<i class="fa fa-angle-left fa-2x"></i>',
-        dots: false,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    autoplay: true
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    autoplay: true
-                }
-            }
-        ]
-    });
-});
-</script>
-    <?php
-}, 9999);
-?>
