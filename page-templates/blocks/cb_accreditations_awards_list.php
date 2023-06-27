4348e62b50bf5098387c10ef7e4156d2d940d276
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-06-27 10:05:33 +0100

    awards list

--- page-templates/blocks/cb_accreditations_awards_list.php
+++ page-templates/blocks/cb_accreditations_awards_list.php
@@ -1,21 +1,28 @@
 <?php
 $list = array();
+$title = '';
 
 if (get_field('type') == 'Accreditations') {
+    $title = 'Accreditations';
     $list = get_field('accreditations','options');        
 }
 else {
+    $title = 'Awards';
     $list = get_field('awards','options');
 }
 ?>
 <section class="awards_list">
     <div class="container-xl">
+        <h2 class="text-blue-400 dot mb-5"><?=$title?></h2>
+        <div class="awards_list__grid">
         <?php
         foreach ($list as $l) {
-            echo '<div>';
-            var_dump($l);
-            echo '</div>';
+            ?>
+            <div><img src="<?=wp_get_attachment_image_url($l['logo'],'large')?>" alt=""></div>
+            <div><?=$l['description']?></div>
+            <?php
         }
         ?>
+        </div>
     </div>
 </section>
