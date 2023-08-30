ed7e26212aa329b9bc2a15cc60f950263f47354c
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-08-30 11:15:44 +0100

    andwis slider

--- page-templates/blocks/cb_andwis.php
+++ page-templates/blocks/cb_andwis.php
@@ -11,20 +11,22 @@
                 $intro = get_sub_field('intro');
                 ?>
             <div class="andwis__card">
-                <img src="<?=$logo?>" alt="<?=$name?>">
-                <div class="andwis__details">
-                    <div class="andwis__icons">
-                        <?php
-                        foreach ($icons as $i) {
-                            ?>
-                        <img src="<?=get_stylesheet_directory_uri()?>/img/andwis/icon__<?=$i?>--emerald.svg" alt="<?=$i?> icon">
+                <div class="andwis__inner">
+                    <img src="<?=$logo?>" alt="<?=$name?>">
+                    <div class="andwis__details">
+                        <div class="andwis__icons">
                             <?php
-                        }
-                        ?>
-                    </div>
-                    <div class="andwis__detail">
-                        <h3><?=$name?></h3>
-                        <p><?=$intro?></p>
+                            foreach ($icons as $i) {
+                                ?>
+                            <img src="<?=get_stylesheet_directory_uri()?>/img/andwis/icon__<?=$i?>--emerald.svg" alt="<?=$i?> icon">
+                                <?php
+                            }
+                            ?>
+                        </div>
+                        <div class="andwis__detail">
+                            <h3><?=$name?></h3>
+                            <p><?=$intro?></p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -48,15 +50,34 @@ jQuery(function($){
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
-        pauseOnHover: true,
-        cssEase: 'linear',
-        fade: true,
-        arrows: true,
-        dots: false,
+        speed: 500,
+        pauseOnHover: false,
+        cssEase: 'ease',
+        arrows: false,
         nextArrow: '<i class="fa fa-angle-right fa-2x"></i>',
-        prevArrow: '<i class="fa fa-angle-left fa-2x"></i>'
+        prevArrow: '<i class="fa fa-angle-left fa-2x"></i>',
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    autoplay: true
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true
+                }
+            }
+        ]
     });
 });
 </script>
     <?php
 }, 9999);
+?>
