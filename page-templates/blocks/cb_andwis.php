ddf87dc297a7f1103852d135342ab0430679ea19
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-11-13 13:25:14 +0000

    andwis block

--- page-templates/blocks/cb_andwis.php
+++ page-templates/blocks/cb_andwis.php
@@ -7,8 +7,11 @@
                 <div class="col-md-8">
                     <div class="mb-4">We bring industry-leading technical building service companies together to offer multi-service solutions for our clients.  Our core beliefs are the fundamental guiding principles that everyone in our group should live by. They reflect what we stand for as a company, and as a team.</div>
                 </div>
+                <div class="col-md-4">
+                    <img src="<?=get_stylesheet_directory_uri()?>/img/andwis_green.svg" alt="">
+                </div>
             </div>
-            <div class="d-flex justify-content-around">
+            <div class="d-flex justify-content-around mb-4">
                 <span class="fs-5 fw-bold dot">Integrity is integral</span>
                 <span class="fs-5 fw-bold dot">Dynamic by design</span>
                 <span class="fs-5 fw-bold dot">Partnership for progress</span>
